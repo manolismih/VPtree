2891637caeb44f1e2aef226b979ad4ce7c7004eb
Emmanouil Michalainas <manolismih@windowslive.com>
2019-10-31 19:38:00 +0200

    cleanup

--- src/vptree_sequential.c
+++ src/vptree_sequential.c
@@ -57,27 +57,8 @@ vptree *buildvp(double *X, int n, int d)
 }
 
 /////////////////////////////////////////////////////////////////////////////
-vptree * getInner(vptree * T)
-{
-    return T->inner;
-}
-
-vptree * getOuter(vptree * T)
-{
-    return T->outer;
-}
-    
-double getMD(vptree * T)
-{
-    return T->md;
-}  
-
-double * getVP(vptree * T)
-{
-    return T->vp;
-}
-
-int getIDX(vptree * T)
-{
-    return T->idx;
-}
+vptree* getInner(vptree * T){    return T->inner;}
+vptree* getOuter(vptree * T){    return T->outer;}
+double getMD(vptree * T){    return T->md;}  
+double* getVP(vptree * T){    return T->vp;}
+int getIDX(vptree * T){    return T->idx;}
