ff8dbfefbaa74e38445375f28cbd6d38b65b9954
Panagiotis Karelis <karelispanagiotis@gmail.com>
2019-11-01 19:51:43 +0200

    Ver.1 pthreads, with branch parallelism, passes the validator

--- src/vptree_pthreads.c
+++ src/vptree_pthreads.c
@@ -1,99 +1,24 @@
-//vptree_pthreads.c
 #include "vptree.h"
 #include "details.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <math.h>
 
-#define NUM_OF_THREADS 2
+//Globally defined variables for easy data access by threads
+int *idArr;
+double *distArr;
+double *Y; //data array
+int N, D;  //data dimensions
 
-typedef struct threadDistCalcArgs
+//Structure for argument passing for threads
+typedef struct threadArgs
 {
     int threadId;
     vptree *node;
-    int start, end, d;
-    int *idArr;
-    double *distArr;
-    double **dataArr;
-} threadDistCalcArgs;
+    int start, end;
+} threadArgs;
 
-///////////////////////////////////////////////////////////////////////////////////
-
-void *threadDistCalc(void *arguments)
-{
-    threadDistCalcArgs *args = (threadDistCalcArgs *)arguments;
-
-    int offset = args->threadId;
-    vptree *node = args->node;
-    int start = args->start;
-    int end = args->end;
-    int d = args->d;
-    int *idArr = args->idArr;
-    double *distArr = args->distArr;
-    double **dataArr = args->dataArr;
-
-    int range = (start - end) / NUM_OF_THREADS;
-
-    int threadStart = start + offset * range;
-    int threadEnd = start + (offset + 1) * range;
-
-    for (int i = threadStart; i < threadEnd; i++)
-        distArr[i] = sqr(node->vp[0] - dataArr[idArr[start]][0]);
-    for (int i = threadStart; i < threadEnd; i++)
-        for (int j = 1; j < d; j++)
-            distArr[i] += sqr(node->vp[j] - dataArr[idArr[start]][j]);
-
-    pthread_exit((void *)0);
-};
-
-///////////////////////////////////////////////////////////////////////////////////
-
-void parallelDistCalc(
-    vptree *node,
-    int start, int end,
-    int n, int d,
-    int idArr[n],
-    double distArr[n],
-    double dataArr[n][d])
-{
-    //Threads declaration
-    pthread_t threads[NUM_OF_THREADS];
-    int rc;       //stores the return value of pthread_create()
-    void *status; //is used as an argument for pthread_join()
-
-    //copy all data to "arguments structure"
-    threadDistCalcArgs *args = malloc(sizeof(threadDistCalcArgs));
-    args->node = node;
-    args->start = start;
-    args->end = end;
-    args->d = d;
-    args->idArr = idArr;
-    args->distArr = distArr;
-    args->dataArr = (double **)dataArr;
-
-    for (int i = 0; i < NUM_OF_THREADS; i++)
-    {
-        args->threadId = i;
-        rc = pthread_create(&threads[i], NULL, threadDistCalc, (void *)args);
-        if (rc)
-        {
-            printf("Error creating thread #%d, Exiting...\n", i);
-            exit(1);
-        }
-    }
-
-    //wait for threads to finish and join
-    for (int i = 0; i < NUM_OF_THREADS; i++)
-    {
-        rc = pthread_join(threads[i], &status);
-        if (rc)
-        {
-            printf("Error joining thread #%d, Exiting\n", i);
-        }
-    }
-};
-
-///////////////////////////////////////////////////////////////////////////////////
+void *threadRecBuildTree(void *argumets);
 
 void recursiveBuildTree(
     vptree *node,
@@ -103,8 +28,12 @@ void recursiveBuildTree(
     double distArr[n],
     double dataArr[n][d])
 {
-    //consider X[ idArr[start] ] as vantage point
-    node->idx = idArr[start];
+    pthread_t threadInner;
+    threadArgs *args;
+    void *status;
+
+    //consider X[ idArr[end] ] as vantage point
+    node->idx = idArr[end];
     node->vp = dataArr[node->idx];
 
     if (start == end)
@@ -114,33 +43,67 @@ void recursiveBuildTree(
         return;
     }
 
-    //calculate distances in parallel
-    parallelDistCalc(node, start, end, n, d, idArr, distArr, dataArr);
+    end--; //end is the vantage point, we're not dealing with it again
 
-    quickSelect((end - start) / 2, distArr, idArr, start, end);
+    for (int i = start; i <= end; i++)
+        distArr[i] = sqr(node->vp[0] - dataArr[idArr[i]][0]);
+    for (int i = start; i <= end; i++)
+        for (int j = 1; j < d; j++)
+            distArr[i] += sqr(node->vp[j] - dataArr[idArr[i]][j]);
+
+    quickSelect((start + end) / 2, distArr, idArr, start, end);
     // now idArr[start .. (start+end)/2] contains the indexes
     // for the points which belong inside the radius (inner)
 
-    node->md = distArr[(start + end) / 2];
+    node->md = sqrt(distArr[(start + end) / 2]);
     node->inner = malloc(sizeof(vptree));
     node->outer = malloc(sizeof(vptree));
-    recursiveBuildTree(node->inner, start, (start + end) / 2, n, d, idArr, distArr, dataArr);
-    recursiveBuildTree(node->outer, (start + end) / 2 + 1, end, n, d, idArr, distArr, dataArr);
-};
 
-///////////////////////////////////////////////////////////////////////////////////
+    //create the arguments to pass
+    args = malloc(sizeof(threadArgs));
+    args->start = start;
+    args->end = end;
+    args->node = node;
+    pthread_create(&threadInner, NULL, threadRecBuildTree, (void *)args);
+    if (end > start)
+        recursiveBuildTree(node->outer, (start + end) / 2 + 1, end, n, d, idArr, distArr, dataArr);
+    else
+        node->outer = NULL;
+
+    //wait for thread to join
+    pthread_join(threadInner, &status);
+}
 
 vptree *buildvp(double *X, int n, int d)
 {
     vptree *root = malloc(sizeof(vptree));
-    int *idArr = malloc(n * sizeof(int));
-    double *distArr = malloc(n * sizeof(double));
+    idArr = malloc(n * sizeof(int));
+    distArr = malloc(n * sizeof(double));
+    Y = X;
+    N = n;
+    D = d;
 
     for (int i = 0; i < n; i++)
         idArr[i] = i;
+
     recursiveBuildTree(root, 0, n - 1, n, d, idArr, distArr, (double(*)[d])X);
 
     free(idArr);
     free(distArr);
     return root;
 }
+
+void *threadRecBuildTree(void *argumets)
+{
+    threadArgs *args = (threadArgs *)argumets;
+
+    recursiveBuildTree(args->node->inner, args->start, (args->start + args->end) / 2, N, D, idArr, distArr, (double(*)[D])Y);
+
+    return (void *)0;
+}
+///////////////////////////////////////////////////////////
+vptree *getInner(vptree *T) { return T->inner; }
+vptree *getOuter(vptree *T) { return T->outer; }
+double getMD(vptree *T) { return T->md; }
+double *getVP(vptree *T) { return T->vp; }
+int getIDX(vptree *T) { return T->idx; }
