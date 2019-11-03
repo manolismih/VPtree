e2ab5f7c7091f915023f66b5e32640bcdb185bcf
Panagiotis Karelis <karelispanagiotis@gmail.com>
2019-11-03 16:23:21 +0200

    Added openmp, passes tester, runs slowly

--- src/vptree_openmp.c
+++ src/vptree_openmp.c
@@ -2,84 +2,88 @@
 #include "details.h"
 #include <stdlib.h>
 #include <math.h>
+#include <stdio.h>
 #include <omp.h>
 
-#define MIN_INTERVAL_PER_THREAD 10
+//Globally defined variables for easy data access by threads
+int *idArr;
+double *distArr;
+double *Y; //data array
+int N, D;  //data dimensions
 
-void recursiveBuildTree(
-    vptree* node, 
-    int start, int end, 
-    int n, int d, 
-    int idArr[n], 
-    double distArr[n], 
-    double dataArr[n][d])
+////////////////////////////////////////////////////////////////////////
+
+void recursiveBuildTree(vptree *node, int start, int end)
 {
+    double(*dataArr)[D] = (double(*)[D])Y;
     //consider X[ idArr[end] ] as vantage point
     node->idx = idArr[end];
-    node->vp  = dataArr[ node->idx ]; 
-    
-    if (start==end)
+    node->vp = dataArr[node->idx];
+    if (start == end)
     {
         node->inner = node->outer = NULL;
         node->md = 0.0;
         return;
     }
     end--; //end is the vantage point, we're not dealing with it again
-    
-    //num_threads()
-    #pragma omp parallel for 
-    for (int i=start; i<=end; i++)
-        distArr[i] = sqr(node->vp[0] -dataArr[ idArr[i] ][0]);
+
     #pragma omp parallel for
-    for (int i=start; i<=end; i++)
-        for (int j=1; j<d; j++)
-            distArr[i] += sqr(node->vp[j] -dataArr[ idArr[i] ][j]);
-    
-    quickSelect( (start+end)/2, distArr, idArr, start, end );
+    for (int i = start; i <= end; i++)
+        distArr[i] = sqr(node->vp[0] - dataArr[idArr[i]][0]);
+    #pragma omp parallel for
+    for (int i = start; i <= end; i++)
+        for (int j = 1; j < D; j++)
+            distArr[i] += sqr(node->vp[j] - dataArr[idArr[i]][j]);
+
+
+
+    quickSelect((start + end) / 2, distArr, idArr, start, end);
     // now idArr[start .. (start+end)/2] contains the indexes
     // for the points which belong inside the radius (inner)
 
-    node->md  = sqrt(distArr[ (start+end)/2 ]);
-    node->inner = malloc( sizeof(vptree) );
-    node->outer = malloc( sizeof(vptree) );
+    node->md = sqrt(distArr[(start + end) / 2]);
+    node->inner = malloc(sizeof(vptree));
+    node->outer = malloc(sizeof(vptree));
     
-    int doThreading = (end-start+1)/2 >= MIN_INTERVAL_PER_THREAD ;
-    if (doThreading) //build inner tree in parallel
+
+    #pragma omp task
     {
-        #pragma omp task
-        recursiveBuildTree(node->inner, start, (start+end)/2, n, d, idArr, distArr, dataArr);
+        recursiveBuildTree(node->inner, start, (start + end) / 2);
     }
+
+    if (end > start)
+        recursiveBuildTree(node->outer, (start + end) / 2 + 1, end);
     else
-        recursiveBuildTree(node->inner, start, (start+end)/2, n, d, idArr, distArr, dataArr);
+        node->outer = NULL;
 
-    if (end>start)
-        recursiveBuildTree(node->outer, (start+end)/2 +1, end,n, d, idArr, distArr, dataArr);
-    else node->outer = NULL;
-    
-    if (doThreading) {
-        #pragma omp taskwait
-    }
-};
+    #pragma omp taskwait
+}
 
-/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 
 vptree *buildvp(double *X, int n, int d)
-{    
-    vptree *root = malloc( sizeof(vptree) );
-    int *idArr = malloc( n*sizeof(int) );
-    double *distArr = malloc ( n*sizeof(double) );
-    
-    for (int i=0; i<n; i++) idArr[i] = i;
-    recursiveBuildTree(root, 0, n-1, n, d, idArr, distArr,  (double (*)[d])X );
-    
+{
+    vptree *root = malloc(sizeof(vptree));
+    idArr = malloc(n * sizeof(int));
+    distArr = malloc(n * sizeof(double));
+    Y = X;
+    N = n;
+    D = d;
+
+    for (int i = 0; i < N; i++)
+        idArr[i] = i;
+
+    recursiveBuildTree(root, 0, n - 1);
+
     free(idArr);
     free(distArr);
     return root;
 }
 
-/////////////////////////////////////////////////////////////////////////////
-vptree* getInner(vptree* T){    return T->inner;}
-vptree* getOuter(vptree* T){    return T->outer;}
-double getMD(vptree* T)    {    return T->md;}  
-double* getVP(vptree* T)   {    return T->vp;}
-int getIDX(vptree* T)      {    return T->idx;}
+///////////////////////////////////////////////////////////
+
+vptree *getInner(vptree *T) { return T->inner; }
+vptree *getOuter(vptree *T) { return T->outer; }
+double getMD(vptree *T) { return T->md; }
+double *getVP(vptree *T) { return T->vp; }
+int getIDX(vptree *T) { return T->idx; }
